9f62495bb4297a9699cab71bb69eb5be42d9c220
Suchkova Maria <maria.13.13@yandex.ru>
2016-03-26 21:14:48 +0300

    добавила сортировку выбором

--- SuchkovaMaria/lab2/sort.cpp
+++ SuchkovaMaria/lab2/sort.cpp
@@ -1,15 +1,68 @@
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 #include <ctime>
-void count_sort(int n, int *array, int min, int max){
-	int *c = new int[max-min+1];
-	for(int i = min; i <= max; ++i) c[i - min] = 0;
-	for(int i = 0; i < n; ++i) ++c[array[i] - min];
-	for(int i = min; i <= max; ++i)
-		for(int j = c[i - min]; j--;)
+void count(int *array, int num, int min_value, int max_value){
+	clock_t start, stop;
+    start = clock();        
+	int *temp = new int[max_value-min_value+1];
+	for(int i = min_value; i <= max_value; i++) temp[i - min_value] = 0;
+	for(int i = 0; i < num; i++) temp[array[i] - min_value]++;
+	for(int i = min_value; i <= max_value; i++)
+		for(int j = temp[i - min_value]; j--;)
 			*array++ = i;
+	stop = clock();
+	double time = double(stop - start) / CLOCKS_PER_SEC;
+	cout<<"The result of sort in ascending order: ";
+	for(int i=0; i<num; i++)
+		cout<<array[i]<< " ";
+	cout<<"\n"<<"Type of sort: counting sort"<<"\n";
+    cout<<"Time: "<<time<<"\n";
+	delete []temp;
+}
+void selection(int *array, int num){
+	clock_t start;
+    clock_t stop;
+    start = clock();
+	int temp;
+    for(int i = 0; i < num; i++)
+    { 
+        int pos = i; 
+        temp = array[i];
+        for(int j = i + 1; j < num; ++j)
+        {
+            if (array[j] < temp) 
+           {
+               pos = j; 
+               temp = array[j]; 
+           }
+        }
+        array[pos] = array[i]; 
+        array[i] = temp; 
+    }
+	stop = clock();
+	double time = double(stop - start) / CLOCKS_PER_SEC;
+	cout<<"The result of sort in descending order: ";
+	for(int i=0; i<num; i++)
+		cout<<array[i]<< " ";
+	cout<<"\n"<<"Type of sort: selection sort"<<"\n";
+    cout<<"Time: "<<time;
 }
 int main()
 {
+	int n;
+	cout<<"Enter size of array"<<"\n";
+	cin>>n;
+	int *mas = new int [n];
+	cout<<"Random array: ";
+	int min=1, max=500;
+	for (int i=0; i<n; i++){
+		mas[i]=rand()%(max-min+1)-min;
+		cout<<mas[i]<<" ";
+	}
+	count(mas, n, min, max);
+	selection(mas,n);
+	delete[]mas;
 	return 0;
 }
 
